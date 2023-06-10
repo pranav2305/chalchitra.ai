1d32d14abd9ebccdc681ad9b4b5a780883537fed
Parth Mittal <mittalparth22@gmail.com>
2023-06-10 15:19:07 +0530

    Translation: init

---
+++ python-scripts/translate.py
@@ -0,0 +1,26 @@
+def translate_text(target: str, text: str) -> dict:
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    if isinstance(text, bytes):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    print("Text: {}".format(result["input"]))
+    print("Translation: {}".format(result["translatedText"]))
+    print("Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+    return result
+
+
+if __name__ == "__main__":
+    translate_text("hi", "AI is the future of mankind.")
